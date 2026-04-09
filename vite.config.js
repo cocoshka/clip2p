49ebf10c2d2d86e2f32cc8a069dbb6335e2d0e71
Marcin Kokoszka <mcocoshka@gmail.com>
2026-04-09 22:06:58 +0200

    Deployment setup

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/clip2p/",
   server: {
     allowedHosts: true,
     cors: true
